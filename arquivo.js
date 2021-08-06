99967a2c7768203e68c85865a8fafe7344f5b3ae
Isabella Bertini <isabella@kenzie.com.br>
2021-08-06 10:59:05 -0300

    Merge varivel, tipagem

--- arquivo.js
+++ arquivo.js
@@ -31,3 +31,51 @@
 
 // ! true => false
 // ! false => true
+
+let x = 5;
+let y = 2;
+let z = 'Letra';
+
+let soma;
+
+let soma = x + y / 2 * 7 + x - y;
+
+// soma = 5 + 2;
+// soma = 7;
+
+//number = numeros
+//string = letras/palavras
+//boolean = verdadeiro ou falso / 1 ou 0
+
+if (x === 1){
+    return true;
+}
+
+function calculaTotal(pr, qtd) {
+  let soma = 0;
+
+  soma = pr * qtd;
+
+  return soma;
+}
+
+let preco = 80;
+let quantidade = 3;
+let desconto = 0;
+let total = calculaTotal(preco, quantidade);
+
+if (total > 200) {
+  desconto = 0.1;
+}
+
+let final = total * (1 - desconto);
+
+let offerPeanutAndChocolateSpecialty = false;
+
+let isAlergicToPeanuts = false;
+let isChocolateLover = true;
+let hasPositiveBalance = true;
+
+if (!isAlergicToPeanuts && isChocolateLover) {
+  offerPeanutAndChocolateSpecialty = true;
+}
